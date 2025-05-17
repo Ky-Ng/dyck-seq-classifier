0d4b86802a157e595e23b49a17c924977757e119
Kyle N <kgng@usc.edu>
2025-05-16 18:03:55 -0700

    Valid and Invalid Dyck Word Data Generation (#1)
    * generate valid parentheses data
    
    * boilerplate GPT code
    
    * wip test smaller dyck language
    
    * raw data generation
    
    * data splits on train test dev unshuffled

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dyck-gpt
 Modelling dependency in LLMs using the Dyck Language
+
+## Data Generation
+```
+src/data_gen.py -o data/input -n 12
+```
+## Splitting Data
+```
+python src/split_data.py -fv data/input/valid_parentheses_n12.txt -fi data/input/invalid_parentheses_n12.txt -n 12 -o ./data/splits
+```
---
+++ src/data_gen.py
@@ -0,0 +1,122 @@
+import argparse
+import math
+import os
+
+
+def generate_dyck_words(n: int) -> list[str]:
+    """
+    Generates all valid dyck words with n open and n close parentheses
+    """
+    def backtrack(base: str, num_open: int, num_close: int) -> None:
+        # Case 1) Completed a dyck word
+        if len(base) == 2*n:
+            ret.append(base)
+            return
+
+        # Case 2) Add an open parentheses i.e. ( => ((
+        if num_open < n:
+            backtrack(base + "(", num_open + 1, num_close)
+
+        # Case 3) Complete a parentheses i.e. (( => (()
+        if num_open > num_close:
+            backtrack(base + ")", num_open, num_close + 1)
+
+    ret = []
+    backtrack(base="", num_open=0, num_close=0)
+    return ret
+
+
+def generate_invalid_dyck_words(n: int) -> list[str]:
+    """
+    Generates all sequences with n open and n close parentheses
+    such that word_1 = "(" and word_{2n} = ")", where word_i = ith position of a string
+
+    Returns:
+        invalid_dyck_words
+
+    Algorithm:
+        1. Generate all sequences with n-1 open and close parentheses (may or may not be balanced)
+        2. Prepend "(" and append ")"
+        3. Remove valid dyck_word
+    """
+    
+    # fix the first and last parentheses to be open
+
+    def is_valid_dyck_word(seq: str) -> bool:
+        if len(seq) % 2 != 0:
+            return False
+
+        num_open_paren = 0
+
+        for c in seq:
+            if c == "(":
+                num_open_paren += 1
+            elif c == ")":
+                num_open_paren -= 1
+            else:
+                print(f"Invalid Character: {c}")
+                return False
+
+            if num_open_paren < 0:
+                return False
+        return True
+
+    def backtrack(seq: str, num_open: int, num_close: int) -> None:
+        """
+        Generate sequences of length 2n with n open and n close parentheses
+        """
+        if len(seq) == 2*(n-1):
+            combinations.append("(" + seq + ")")
+        if num_open < n-1:
+            backtrack(seq + "(", num_open + 1, num_close)
+        if num_close < n-1:
+            backtrack(seq + ")", num_open, num_close + 1)
+    
+    # Populate combinations with all combos of n "(" and n ")"
+    combinations = []
+    backtrack("", num_open=0, num_close=0)
+    
+    # return only the invalid ones
+    invalid_dyck_words = [s for s in combinations if not is_valid_dyck_word(s)]
+    return invalid_dyck_words
+
+def catalan(n):
+    return math.comb(2*n, n) // (n+1)
+
+def main():
+    print("Initializing Data Generation of Dyck Words")
+
+    # Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--length", type=int,
+                        required=True, help="Number of Open Parentheses")
+    parser.add_argument("-o", "--output_dir", type=str,
+                        required=False, help="Output directory to write results")
+
+    args = parser.parse_args()
+    n = args.length
+    dir = args.output_dir
+
+    valid_dyck_words = generate_dyck_words(n)
+    invalid_dyck_words = generate_invalid_dyck_words(n)
+
+    # Write out Dyck words
+    if dir:
+        # Create output directories if needed
+        os.makedirs(dir, exist_ok=True)
+
+        with open(os.path.join(dir, f"valid_parentheses_n{n}.txt"), mode='w') as f:
+            for word in valid_dyck_words:
+                f.write(word + "\n")
+        
+        with open(os.path.join(dir, f"invalid_parentheses_n{n}.txt"), mode='w') as f:
+            for word in invalid_dyck_words:
+                f.write(word + "\n")
+
+        print(f"Writing to {dir}")
+
+    print(f"Generated {len(valid_dyck_words)} out of {catalan(n)} combinations; Generated {len(invalid_dyck_words)} invalid words")
+
+
+if __name__ == "__main__":
+    main()
